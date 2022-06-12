6ebe3beb2652fe83a5fe3483718119af77fd2870
yousufmiah <yousufweb99@gmail.com>
2022-06-12 07:58:34 +0600

    edited

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,18 +3,18 @@ import selfPic from "../assets/images/Md. Yousuf Miah.jpg";
 
 const About = () => {
   return (
-    <div className="">
+    <div className="mx-auto">
       <div className="text-center my-24 ">
         <h1 className="text-xl md:text-6xl text-primary font-bold">About Me</h1>
       </div>
-      <div className="container mx-auto flex-col md:flex-row flex justify-between my-8 ">
-        <div className="w-full md:w-1/2 border rounded m-10">
-          <div className="w-full  h-92 text-center">
+      <div className="container mx-auto flex flex-col md:flex-row gap-16 justify-between my-8 ">
+        <div className="w-full md:w-1/2 mx-auto border rounded ">
+          <div className="w-full h-full flex justify-center items-center mx-auto  ">
             <img className=" z-10 rounded-full mx-auto" src={selfPic} alt="" />
           </div>
         </div>
-        <div className="w-full md:w-1/2 border rounded m-10">
-          <div className="  text-justify text-xl leading-10  p-12">
+        <div className="w-full md:w-1/2 mx-auto border rounded ">
+          <div className="  text-justify text-xl leading-10 p-12">
             <p>
               Really a great message by the grace of Almighty{" "}
               <strong> "Allah" </strong> inform you about me. This is
@@ -40,13 +40,15 @@ const About = () => {
           </div>
         </div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#176F6B"
-          fill-opacity="1"
-          d="M0,32L40,37.3C80,43,160,53,240,69.3C320,85,400,107,480,128C560,149,640,171,720,170.7C800,171,880,149,960,154.7C1040,160,1120,192,1200,224C1280,256,1360,288,1400,304L1440,320L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
-        ></path>
-      </svg>
+      <div className="w-full mx-auto p-8">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#176F6B"
+            fill-opacity="1"
+            d="M0,32L40,37.3C80,43,160,53,240,69.3C320,85,400,107,480,128C560,149,640,171,720,170.7C800,171,880,149,960,154.7C1040,160,1120,192,1200,224C1280,256,1360,288,1400,304L1440,320L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -2,14 +2,15 @@ import React from "react";
 import yousufImg from "../../assets/images/yousuf_miah.jpg";
 import { BsGithub } from "react-icons/bs";
 import { BsLinkedin } from "react-icons/bs";
+import { FaTwitterSquare } from "react-icons/fa";
 
 const Landing = () => {
   return (
     <div className="min-h-screen">
-      <div className="hero lg:h-[-60vh] bg-base-200 py-60">
+      <div className="hero lg:h-[-60vh] bg-base-200 pt-20 md:py-60">
         <div className="hero-content flex-col lg:flex-row">
           <div
-            className="max-w-5xl p-20"
+            className="max-w-5xl p-20 "
             data-aos="fade-right"
             data-aos-duration="1000"
           >
@@ -17,7 +18,7 @@ const Landing = () => {
             <h1 className="sm:text-3xl md:text-5xl font-bold text-primary">
               Professional Web Developer || MERN
             </h1>
-            <p className="py-4 max-w-xl text-xl">
+            <p className="py-4 max-w-xl text-2xl">
               I like to effort my best service as your requirements which will
               be acceptable for the success point.
             </p>
@@ -26,16 +27,24 @@ const Landing = () => {
               <a
                 target="blank"
                 href="https://github.com/yousufmiah"
-                className="text-3xl text-primary rounded cursor-pointer mx-3"
+                className="text-5xl text-primary rounded cursor-pointer mx-3"
               >
                 <BsGithub />
               </a>
               <a
                 target="blank"
                 href="https://www.linkedin.com/in/yousufmiah/"
-                className="text-3xl text-primary rounded cursor-pointer mx-3"
+                className="text-5xl text-primary rounded cursor-pointer mx-3"
               >
                 <BsLinkedin />
+              </a>
+
+              <a
+                target="blank"
+                href="https://twitter.com/yousufmiahbd"
+                className="text-5xl text-primary cursor-pointer mx-3"
+              >
+                <FaTwitterSquare />
               </a>
             </div>
 
@@ -43,7 +52,7 @@ const Landing = () => {
               href="https://drive.google.com/file/d/1AfYUIwAyfkllf9jTuntc46xQneGXMFm8/view?usp=sharing"
               target="_blank"
             >
-              <button className="btn btn-primary"> Download Resume</button>
+              <button className="btn btn-primary px-8">Download Resume</button>
             </a>
           </div>
           <div
